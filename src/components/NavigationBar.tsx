e307b4d28b5640dff21772d5704333cb6b62ed9a
arnavgupta <arnav@agupta.org>
2023-11-25 16:51:40 -0500

    fixed navbar menu bug where text was not showing

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -79,7 +79,7 @@ const NavigationBar = () => {
                 component={RouterLink}
                 to={page.link}
                 onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                sx={{ my: 2, color: 'black', display: 'block' }}
               >
                 {page.title}
               </Button>
